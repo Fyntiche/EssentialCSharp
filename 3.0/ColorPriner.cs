7f10a5c8e2d0687572829672b6a606777252f90f
Ivan <ivaneremov@icloud.com>
2019-12-23 09:34:47 +0300

    Создайте класс Printer. В теле класса создайте метод void Print(string value), который выводит на экран значение аргумента. Реализуйте возможность того, чтобы в случае наследования от данного класса других классов, и вызове соответствующего метода их экземпляра, строки, переданные в качестве аргументов методов, выводились разными цветами. Обязательно используйте приведение типов.

---
+++ 3.0/ColorPriner.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._0
+{
+    class ColorPriner : Printer
+    {
+        private ConsoleColor color;
+
+        public ColorPriner(ConsoleColor color)
+        {
+            this.color = color;
+        }
+
+        public void Print(string value)
+        {
+            Console.ForegroundColor = color;
+            base.Print(value);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ 3.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ColorPriner colorPriner = new ColorPriner(ConsoleColor.Red);
+            colorPriner.Print("hello");
+
+            Printer printer = colorPriner;
+            printer.Print("my");
+
+            ColorPriner colorPriner1 = new ColorPriner(ConsoleColor.Green);
+            colorPriner1.Print("world");
+
+            Console.ReadKey();
+        }
+    }
+}
