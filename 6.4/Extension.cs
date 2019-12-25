b918798293b27bccbb3ff65b9efd161a92af1c90
Ivan Eryomov <ivaneremov@icloud.com>
2019-12-25 19:06:25 +0300

    создать расширяющий метод для целочисленного массива, который сортирует элементы массива по возрастанию.

---
+++ 6.4/Extension.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._4
+{
+    static class Extension
+    {
+        public static int[] MySort(this int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+}
---
+++ 6.4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mass = { 1, 5, 10, 45 };
+            mass.MySort();
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine(mass[i]);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
