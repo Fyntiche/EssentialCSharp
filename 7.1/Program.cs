b1bae7a9c177a6875dfe30e3ea1d6694c9371912
Ivan Eryomov <ivaneremov@icloud.com>
2019-12-25 21:03:46 +0300

    Создайте структуру с именем - Notebook. Поля структуры: модель, производитель, цена. В структуре должен быть реализован конструктор для инициализации полей и метод для вывода содержимого полей на экран.

---
+++ 7.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._1
+{
+    class Program
+    {
+        struct Notebook
+        {
+            private string model;
+
+            private string firm;
+
+            private int price;
+
+            public Notebook(string model, string firm, int price)
+            {
+                this.model = model;
+                this.firm = firm;
+                this.price = price;
+            }
+
+            public void Show()
+            {
+                Console.WriteLine("{0} {1} по цене {2}", firm, model, price);
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
