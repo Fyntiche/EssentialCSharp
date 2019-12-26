ca9de0e3dc86051bbfb5af451797a47b976bc732
Ivan <ivaneremov@icloud.com>
2019-12-26 16:29:39 +0300

    Создайте статический класс с методом void Print (string stroka, int color), который выводит на экран строку заданным цветом. Используя перечисление, создайте набор цветов, доступных пользователю. Ввод строки и выбор цвета предоставьте пользователю.

---
+++ 8.1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._1
+{
+    class Program
+    {
+        public enum MyConsoleColor
+        {
+
+            Black = 0,
+            DarkBlue = 1,
+            DarkGreen = 2,
+            DarkCyan = 3,
+            DarkRed = 4,
+            DarkMagenta = 5,
+            DarkYellow = 6,
+            Gray = 7,
+            DarkGray = 8,
+            Blue = 9,
+            Green = 10,
+            Cyan = 11,
+            Red = 12,
+            Magenta = 13,
+            Yellow = 14,
+            White = 15
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите текст");
+            string stroka = Console.ReadLine();
+
+            Console.WriteLine("Введите цвет");
+            int.TryParse(Console.ReadLine(), out int collor);
+
+            ConsolePrint.Print(stroka, collor);
+            Console.ReadLine();
+        }
+    }
+}
