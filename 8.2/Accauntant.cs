4481d2c5515c1351ce4d9c54dd8eaeed9dc9a9ec
Ivan <ivaneremov@icloud.com>
2019-12-26 16:43:14 +0300

    Создайте перечисление, в котором будут содержаться должности сотрудников как имена констант. Присвойте каждой константе значение, задающее количество часов, которые должен отработать сотрудник за месяц. Создайте класс Accauntant с методом bool AskForBonus(Post worker, int hours), отражающее давать или нет сотруднику премию. Если сотрудник отработал больше положеных часов в месяц, то ему положена премия.

---
+++ 8.2/Accauntant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static _8._2.Program;
+
+namespace _8._2
+{
+    class Accauntant
+    {
+        public bool AskForBonus(Post worker, int hour)
+        {
+            if ((int)worker <= hour)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ 8.2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8._2
+{
+
+    
+
+    class Program
+    {
+
+        public enum Post
+        {
+            Rang1 = 10,
+            Rang2 = 20,
+            Rang3 = 30,
+            Rang4 = 40
+            }
+
+        static void Main(string[] args)
+        {
+            Accauntant accauntant = new Accauntant();
+            Console.WriteLine(accauntant.AskForBonus(Post.Rang4, 39));
+            Console.ReadKey();
+        }
+    }
+}
