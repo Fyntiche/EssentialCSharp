4421a9a9a4e1c899690be6c26a2cfa9db6a873c8
Ivan <ivaneremov@icloud.com>
2019-12-26 09:27:53 +0300

    Создайте класс MyClass и структуру MyStruct, которые содержат в себе поля public string change. В классе Program создайте два метода: - static void ClassTaker(MyClass myClass), который присваивает полю change экземпляра myClass значение «изменено». - static void StruktTaker(MyStruct myStruct), который присваивает полю change экземпляра myStruct значение «изменено». Продемонстрируйте разницу в использовании классов и структур, создав в методе Main() экземпляры структуры и класса. Измените, значения полей экземпляров на «не изменено», а затем вызовите методы ClassTaker и StruktTaker. Выведите на экран значения полей экземпляров. Проанализируйте полученные результаты.

---
+++ 7.3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._3
+{
+    class MyClass
+    {
+        public string change;
+    }
+
+    struct MyStruct
+    {
+        public string change;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            MyStruct myStruct = new MyStruct();
+
+            myClass.change = "не изменено";
+            myStruct.change = "не изменено";
+
+            Console.WriteLine(myClass.change);
+            Console.WriteLine(myStruct.change);
+
+            Console.ReadLine();
+
+        }
+
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StructTaket(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
+    }
+}
