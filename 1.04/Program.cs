a9cce49947a252005110173b3fab078355f06560
Ivan <ivaneremov@icloud.com>
2019-12-27 10:06:52 +0300

    Найдите и исправьте ошибки в коде

---
+++ 1.04/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Less01_task02 
+{ 
+    class Program 
+    { 
+        public static void Main() 
+        { 
+            MyClass myClass = new MyClass(); 
+            Console.WriteLine(myClass.X); 
+        } 
+    } 
+    
+    class MyClass 
+    { 
+        int x; 
+        public int X 
+        { 
+            get { 
+                return x; 
+            } 
+        } 
+        public MyClass(int x) 
+        { 
+            this.x = x; 
+        } 
+        
+        public MyClass() 
+        { } 
+    } 
+}
