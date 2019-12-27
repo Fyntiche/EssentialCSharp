1249ecb4531fa8c73b92efe41af59a25bc44748d
Ivan <ivaneremov@icloud.com>
2019-12-27 10:13:47 +0300

    В классе MyClass реализуйте метод IsLuckyTicket, который должен принимать на вход строковое представление шестизначного номера лотерейного билета, и возвращать значение, является ли он «счастливым» - то есть, равна ли сумма первых трех цифр сумме остальных трех. Например: “123006” – счастливый, а “345123” – нет. На любую некорректную входную строку метод должен возвращать значение false.

---
+++ 2.03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Program 
+{ 
+    public class MyClass 
+    { 
+        public static int SumDigits(int number)
+        { 
+            string s = number.ToString(); 
+            if (s.Length == 1) 
+                s = s.Insert(0, "00"); 
+            else 
+            if (s.Length == 2) 
+                s = s.Insert(0, "0"); 
+            return Convert.ToInt32(s[0].ToString()) + Convert.ToInt32(s[1].ToString()) + Convert.ToInt32(s[2].ToString()); 
+        } 
+        
+        public bool IsLuckyTicket(string ticket) 
+        { 
+            if (ticket.Length != 6) return false; 
+            int number; 
+            if (!Int32.TryParse(ticket, out number)) 
+                return false; 
+            return (SumDigits(number / 1000) == SumDigits(number % 1000)); 
+        } 
+    } 
+}
