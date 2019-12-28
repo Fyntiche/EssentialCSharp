ae326c7b2272c053126dca3ec8a59792fabfc904
Ivan Eryomov <ivaneremov@icloud.com>
2019-12-28 17:10:15 +0300

    Создайте анонимный метод, который принимает в качестве параметров три целочисленных аргумента и возвращает среднее арифметическое этих аргументов.

---
+++ 9.1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1
+{
+    class Program
+    {
+        public delegate double Sum(int a, int b, int c);
+        static void Main(string[] args)
+        {
+            Sum sum = delegate(int a, int b, int c) {
+                return (a + b + c) / 3; 
+                    };
+            Console.WriteLine(sum(5,6,10));
+            Console.Read();
+        }
+    }
+}
