8d9f9e67c30320cbb574e102caa8ff6bd4fbbecb
Ivan Eryomov <ivaneremov@icloud.com>
2019-12-28 17:33:15 +0300

    Создайте четыре лямбда оператора для выполнения арифметических действий: (Add – сложение, Sub – вычитание, Mul – умножение, Div – деление). Каждый лямбда оператор должен принимать два аргумента и возвращать результат вычисления. Лямбда оператор деления должен делать проверку деления на ноль. Написать программу, которая будет выполнять арифметические действия, указанные пользователем.

---
+++ 9.2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._2
+{
+    class Program
+    {
+        public delegate int func(int x, int y);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            int.TryParse(Console.ReadLine(), out int x);
+            Console.WriteLine("Введите второе число");
+            int.TryParse(Console.ReadLine(), out int y);
+            Console.WriteLine("Введите действие (add, sub, mul, div)");
+            string maths = Console.ReadLine();
+            switch (maths)
+            {
+                case "add": func add = (int a, int b) => a + b;
+                    Console.WriteLine(add(x, y));
+                    break;
+                case "sub": func sum = (int a, int b) => a - b;
+                    Console.WriteLine(sum(x, y));
+                    break;
+                case "mul": func mul = (int a, int b) => a * b;
+                    Console.WriteLine(mul(x, y));
+                    break;
+                case "div":
+                    if (y==0)
+                    {
+                        Console.WriteLine("Деление на ноль");
+                        break;
+                    }
+                    else
+                    {
+                        func div = (int a, int b) => a / b;
+                        Console.WriteLine(div(x, y));
+                        
+                        Console.WriteLine(div);
+                        break;
+                    }
+                default: Console.WriteLine("Такой операции не существует"); 
+                    break;
+                    
+            }
+            Console.ReadLine();
+        }
+    }
+}
