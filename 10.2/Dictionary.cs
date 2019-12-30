53fd1d35ea71edc9ed308301fd039a338f86e363
Ivan <ivaneremov@icloud.com>
2019-12-30 17:58:45 +0300

    попровил ошибки

---
+++ 10.2/Dictionary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._2
+{
+    class Dictionary<T, R> : IMyDictionary<T>
+    {
+        Dictionary<T, R> dictionary = new Dictionary<T, R>();
+        public T this[int index] => throw new NotImplementedException();
+
+        public void Add(T keyValues)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count()
+        {
+            return 0;
+        }
+    }
+}
---
+++ 10.2/IMyDictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._2
+{
+    interface IMyDictionary<T>
+    {
+        void Add(T keyValues);
+
+        T this[int index] { get; }
+
+        int Count();
+    }
+}
