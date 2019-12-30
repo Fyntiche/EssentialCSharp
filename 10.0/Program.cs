9baa2897ef3739b86a8cb9ae08f96fc4600de605
Ivan <ivaneremov@icloud.com>
2019-12-30 11:48:43 +0300

    Создайте класс MyClass<T>, содержащий статический фабричный метод – T FacrotyMethod(), который будет порождать экземпляры типа, указанного в качестве параметра типа (указателя места заполнения типом – Т).

---
+++ 10.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10._0
+{
+    class MyClass<T> where T : new()  /*where T : new() - ограничение параметра типа Т, 
+                                         * означает что в качестве типа Т можно поставить 
+                                         * только тот тип у которого есть открытый конструктор 
+                                         * по умолчанию */
+    {
+        public static T FacrotyMethod() //Обобщенный метод возвращающий значение типа указателя места заполнения типом 
+        {
+            return new T();
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            int i = MyClass<int>.FacrotyMethod(); //Закрываем экземпляр класса MyClass типом int и записываем результат выполнения метода FacrotyMethod в локальную переменную i
+
+            Console.WriteLine(i.GetType().Name); //Метод отображает имя типа переменной i
+
+            Program p = MyClass<Program>.FacrotyMethod(); //Создаем переменную типа Program
+
+            Console.WriteLine(p.GetType().Name); //Метод отображает имя типа переменной p
+
+
+            Console.ReadKey();
+        }
+    }
+}
